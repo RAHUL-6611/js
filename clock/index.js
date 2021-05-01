aaf93a98689cf04d3951ff8cfa6dea20b01afb21
rahul <rahulparmar7654321@gmail.com>
2021-05-01 18:21:58 +0530

    one

---
+++ clock/index.js
@@ -0,0 +1,25 @@
+const h = document.getElementById(hour);
+const m = document.getElementById(minute);
+const s = document.getElementById(second);
+
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30 * htime + mtime / 2 + stime / 12;
+    mrotation = 6 * mtime + stime / 10;
+    srotation = 6 * stime;
+
+    hour.style.backgroundColor = 'black';
+    hour.style.height = 100 + "%"; 
+    hour.style.transform = 'rotate(${hrotation}deg)';
+    minute.style.transform = 'rotate(${mrotation}deg)';
+    second.style.transform = 'rotate(${srotation}deg)';
+
+    // h.style.transform = 'rotate(${hrotation}deg)';
+    // m.style.transform = 'rotate(${mrotation}deg)';
+    // s.style.transform = 'rotate(${srotation}deg)';
+
+
+}, 1000);
---
+++ emailvalidator/script.js
@@ -0,0 +1,18 @@
+const Name = document.getElementById('name');
+const password = document.getElementById('password');
+const form  = document.getElementById('form');
+const errorElement = document.getElementById('error');
+
+form.addEventListener('submit', (e) => {
+
+    let messages = [];
+
+    if (Name.value === '' || Name.value == null){
+        messages.push('Name is required');
+    }
+
+    if (messages.length > 0 ){
+        e.preventDefault();
+        errorElement.innerText = messages.join(',');
+    }
+})
